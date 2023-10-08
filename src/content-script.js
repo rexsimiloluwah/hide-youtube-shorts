9d4d0d0cf3b4d4682dc730d799b12cb0c2955165
rexsimiloluwah <adetoyosi2001@gmail.com>
2023-10-08 22:37:15 +0100

    chore: added feature for removing shorts tab from navigation

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,6 @@
 setTimeout(() => {
-  const mediaElements = document.getElementsByClassName("ytd-rich-item-renderer")
+  try{
+    const mediaElements = document.getElementsByClassName("ytd-rich-item-renderer")
 
   for (const element of mediaElements){
     const isShortElement = element.getAttribute("is-short")
@@ -25,6 +26,18 @@ setTimeout(() => {
         element.appendChild(divElement)
     }
   }
-}, 2000)
-
+  }catch{
+    console.error("Could not hide YouTube shorts")
+  }
+  
+  try{
+    const navigationEl = document.querySelector("ytd-guide-section-renderer.ytd-guide-renderer")
+    const shortsTabEl = navigationEl.querySelector(
+        "#items .ytd-guide-section-renderer:nth-child(2)"
+    );
+    shortsTabEl.remove()
+  }catch{
+    console.log("Could not hide YouTube shorts tab")
+  }
+}, 5000)
 
